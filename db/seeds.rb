96b64fd0c088cc443c8642b20ce725fb7b7a1398
Luke Pfeiffer <lukerpfeiffer@gmail.com>
2017-09-15 22:45:58 -0400

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Creating user"
+
+User.create(email: "email@example.com", password: "password", confirmed_email: true, access_level: 1)
+
+puts "Create admin"
+admin = User.create(email: "admin@example.com", password: "password", confirmed_email: true, access_level: 4)
+
+puts "Creating faqs"
+5.times do |n|
+  Faq.create(question: "FAQ #{n+1}", answer: "Some answer #{n+1}")
+end
+
+puts "Creating super_user"
+admin = User.create(email: "super@example.com", password: "password", confirmed_email: true, access_level: 5)
