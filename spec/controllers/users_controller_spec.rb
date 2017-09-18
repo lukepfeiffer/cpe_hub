716963b64eda25e0f017fd041dc6bf54c914de13
Luke Pfeiffer <lukerpfeiffer@gmail.com>
2017-09-18 08:24:36 -0400

    Test user mailer

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,4 +9,19 @@ describe UsersController, type: :controller do
       expect(User.last.confirmed_email).to be true
     end
   end
+
+  describe "#create" do
+    before do
+      @user_params = {user: {username: "name", email: "email@blue", password: "password", password_confirmation: "password" } }
+    end
+
+    it 'sends an email to confirm' do
+      ActionMailer::Base.deliveries = []
+      post :create, @user_params
+
+      mail = ActionMailer::Base.deliveries.last
+      expect(mail.subject).to include "Registration Confirmation"
+      expect(mail.to).to include "email@blue"
+    end
+  end
 end
