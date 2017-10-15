763d95b6aecfe7530d211725af9a2216c75434ff
Luke Pfeiffer <lukerpfeiffer@gmail.com>
2017-10-15 10:13:57 -0400

    Basic homepage styles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
+  expose :courses do
+    Course.all.last(20)
+  end
+
+  expose :discussions do
+    Discussion.all.last(10)
+  end
+
+  expose :professors do
+    Professor.all.last(5)
+  end
+
   def home
   end
 
